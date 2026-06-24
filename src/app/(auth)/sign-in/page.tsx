1fd2cf794c27b22ca4b014b5b837d0f5b7ff57d2
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2026-06-24 22:53:46 +0300

    Sign-in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import SignInForm from './form/SignInForm'
+
+export default function SignInPage() {
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <SignInForm />
+        </div>
+    )
+
+}
